c8d57ffbaa437235f3a67241e48d3d191c6c964d
Matthias Mayr <matthias.mayr@unibw.de>
2025-08-16 17:23:58 +0000

    Unify Doxygen style

--- src/core/linalg/src/sparse/4C_linalg_utils_sparse_algebra_manipulation.hpp
+++ src/core/linalg/src/sparse/4C_linalg_utils_sparse_algebra_manipulation.hpp
@@ -23,17 +23,17 @@ FOUR_C_NAMESPACE_OPEN
 namespace Core::LinAlg
 {
   /*!
-   \brief Communicate a vector to a different map
-
-   Values of source are copied to target where maps don't have to match.
-   Prerequisite: Either the map of source OR the map of target has to be unique
-   (will be tested)
-   \warning When source is overlapping (and therefore target is unique), values
-   in the overlapping region are inserted into the target on a first come
-   first serve basis, meaning they should be equal in the source to
-   be deterministic
-   \param source (in) : source vector values are taken from
-   \param target (out): target vector values will be inserted in
+   * \brief Communicate a vector to a different map
+   *
+   * Values of source are copied to target where maps don't have to match.
+   * Prerequisite: Either the map of source OR the map of target has to be unique
+   * (will be tested)
+   * \warning When source is overlapping (and therefore target is unique), values
+   * in the overlapping region are inserted into the target on a first come
+   * first serve basis, meaning they should be equal in the source to
+   * be deterministic
+   * \param source (in) : source vector values are taken from
+   * \param target (out): target vector values will be inserted in
    */
   void export_to(
       const Core::LinAlg::MultiVector<double>& source, Core::LinAlg::MultiVector<double>& target);
@@ -113,25 +113,25 @@ namespace Core::LinAlg
   std::shared_ptr<Core::LinAlg::Graph> enrich_matrix_graph(const SparseMatrix& A, int power);
 
   /*!
-   \brief split a matrix into a 2x2 block system
-
-   Splits a given matrix into a 2x2 block system. All values on entry have to be
-   nullptr except the given rowmap(s) / domainmap(s) and matrix A.
-   Note that either A11rowmap or A22rowmap or both have to be nonzero!
-   Note that either A11domainmap or A22domainmap or both have to be nonzero!
-   In case both rowmaps / domainmaps are supplied they have to be an exact and
-   nonoverlapping split of A->RowMap() / A->DomainMap().
-   Matrix blocks are fill_complete() on exit.
-
-   \param A            : Matrix A on input
-   \param A11rowmap    : rowmap of A11 or null
-   \param A22rowmap    : rowmap of A22 or null
-   \param A11domainmap : domainmap of A11 or null
-   \param A22domainmap : domainmap of A22 or null
-   \param A11          : on exit matrix block A11
-   \param A12          : on exit matrix block A12
-   \param A21          : on exit matrix block A21
-   \param A22          : on exit matrix block A22
+   * \brief split a matrix into a 2x2 block system
+   *
+   * Splits a given matrix into a 2x2 block system. All values on entry have to be
+   * nullptr except the given rowmap(s) / domainmap(s) and matrix A.
+   * Note that either A11rowmap or A22rowmap or both have to be nonzero!
+   * Note that either A11domainmap or A22domainmap or both have to be nonzero!
+   * In case both rowmaps / domainmaps are supplied they have to be an exact and
+   * nonoverlapping split of A->RowMap() / A->DomainMap().
+   * Matrix blocks are fill_complete() on exit.
+   *
+   * \param A            : Matrix A on input
+   * \param A11rowmap    : rowmap of A11 or null
+   * \param A22rowmap    : rowmap of A22 or null
+   * \param A11domainmap : domainmap of A11 or null
+   * \param A22domainmap : domainmap of A22 or null
+   * \param A11          : on exit matrix block A11
+   * \param A12          : on exit matrix block A12
+   * \param A21          : on exit matrix block A21
+   * \param A22          : on exit matrix block A22
    */
   bool split_matrix2x2(std::shared_ptr<Core::LinAlg::SparseMatrix> A,
       std::shared_ptr<Core::LinAlg::Map>& A11rowmap, std::shared_ptr<Core::LinAlg::Map>& A22rowmap,
@@ -156,15 +156,16 @@ namespace Core::LinAlg
   void split_matrixmxn(
       const Core::LinAlg::SparseMatrix& ASparse, Core::LinAlg::BlockSparseMatrixBase& ABlock);
 
-  /*! \brief Split matrix in either 2x2 or NxN blocks (with N>2)
-
-    Split given sparse matrix into 2x2 or NxN block matrix and return result as templated
-    BlockSparseMatrix. The MultiMapExtractor's provided have to be 2x2 or NxN maps, otherwise
-    this method will throw an error.
-
-    \warning This is an expensive operation!
-
-    \note This method will NOT call Complete() on the output BlockSparseMatrix.
+  /*!
+   * \brief Split matrix in either 2x2 or NxN blocks (with N>2)
+   *
+   * Split given sparse matrix into 2x2 or NxN block matrix and return result as templated
+   * BlockSparseMatrix. The MultiMapExtractor's provided have to be 2x2 or NxN maps, otherwise
+   * this method will throw an error.
+   *
+   * \warning This is an expensive operation!
+   *
+   * \note This method will NOT call Complete() on the output BlockSparseMatrix.
    */
   template <class Strategy>
   std::shared_ptr<Core::LinAlg::BlockSparseMatrix<Strategy>> split_matrix(
@@ -203,88 +204,86 @@ namespace Core::LinAlg
       Core::LinAlg::SparseMatrix& mat, const Core::LinAlg::Vector<double>& diag);
 
   /*!
-   \brief Split an Core::LinAlg::Map and return the part complementary to \c Agiven
-
-   Splits \c Amap into 2 maps, where one is given on input and the other map
-   is created as complementary map. The complementary map is returned.
-
-   \param[in] Amap      : Map to split on input
-   \param[in] Agiven    : on entry submap that is given and part of Amap
-   \return the remainder map of Amap that is not overlapping with Agiven
+   * \brief Split an Core::LinAlg::Map and return the part complementary to \c Agiven
+   *
+   * Splits \c Amap into 2 maps, where one is given on input and the other map
+   * is created as complementary map. The complementary map is returned.
+   *
+   * \param[in] Amap      : Map to split on input
+   * \param[in] Agiven    : on entry submap that is given and part of Amap
+   * \return the remainder map of Amap that is not overlapping with Agiven
    */
   std::shared_ptr<Core::LinAlg::Map> split_map(
       const Core::LinAlg::Map& Amap, const Core::LinAlg::Map& Agiven);
 
   /*!
-   \brief merges two given Core::LinAlg::Maps
-
-   merges input map1 and input map2, both of which have to be unique,
-   but may be overlapping, to a new map and returns std::shared_ptr to it.
-
-   \param map1         : one map to be merged
-   \param map2         : the other map to be merged
-   \param allowoverlap : when set to false, an error is thrown if the result
-   map is overlapping (default = true, overlap allowed)
-   \return the (sorted) merged map of input maps map1 and map2
+   * \brief Merge two given Core::LinAlg::Maps into one
+   *
+   * Merges input map1 and input map2, both of which have to be unique,
+   * but may be overlapping, to a new map and returns std::shared_ptr to it.
+   *
+   * \param map1         : one map to be merged
+   * \param map2         : the other map to be merged
+   * \param allowoverlap : when set to false, an error is thrown if the result
+   * map is overlapping (default = true, overlap allowed)
+   * \return the (sorted) merged map of input maps map1 and map2
    */
   std::shared_ptr<Core::LinAlg::Map> merge_map(
       const Core::LinAlg::Map& map1, const Core::LinAlg::Map& map2, bool overlap = true);
 
   /*!
-   \brief find the intersection set of two given Core::LinAlg::Maps
-
-   Find the insection set of input map1 and input map2.
-
-   \param map1         : first map
-   \param map2         : second map
-   \return the (sorted) intersection map of input maps map1 and map2
+   * \brief find the intersection set of two given Core::LinAlg::Maps
+   *
+   * Find the insection set of input map1 and input map2.
+   *
+   * \param map1         : first map
+   * \param map2         : second map
+   * \return the (sorted) intersection map of input maps map1 and map2
    */
   std::shared_ptr<Core::LinAlg::Map> intersect_map(
       const Core::LinAlg::Map& map1, const Core::LinAlg::Map& map2);
 
 
   /*!
-   \brief merges two given Core::LinAlg::Maps
-
-   merges input map1 and input map2 (given as std::shared_ptr), both of which
-   have to be unique, but may be overlapping, to a new map and returns
-   std::shared_ptr to it. The case that one or both input std::shared_ptrs are null is
-   detected and handled appropriately.
-
-   \param map1         : one map to be merged
-   \param map2         : the other map to be merged
-   \param allowoverlap : when set to false, an error is thrown if the result
-   map is overlapping (default = true, overlap allowed)
-   \return the (sorted) merged map of input maps map1 and map2
+   * \brief merges two given Core::LinAlg::Maps
+   *
+   * merges input map1 and input map2 (given as std::shared_ptr), both of which
+   * have to be unique, but may be overlapping, to a new map and returns
+   * std::shared_ptr to it. The case that one or both input std::shared_ptrs are null is
+   * detected and handled appropriately.
+   *
+   * \param map1         : one map to be merged
+   * \param map2         : the other map to be merged
+   * \param allowoverlap : when set to false, an error is thrown if the result map is overlapping
+   * (default = true, overlap allowed)
+   * \return the (sorted) merged map of input maps map1 and map2
    */
   std::shared_ptr<Core::LinAlg::Map> merge_map(const std::shared_ptr<const Core::LinAlg::Map>& map1,
       const std::shared_ptr<const Core::LinAlg::Map>& map2, bool overlap = true);
 
   /*!
-     \brief split a vector into 2 non-overlapping pieces (std::shared_ptr version)
-
-     \param xmap    : map of vector to be split
-     \param x       : vector to be split
-     \param x1map   : map of first vector to be extracted
-     \param x1      : first vector to be extracted
-     \param x2map   : map of second vector to be extracted
-     \param x2      : second vector to be extracted
-
-     */
+   * \brief split a vector into 2 non-overlapping pieces (std::shared_ptr version)
+   *
+   * \param xmap    : map of vector to be split
+   * \param x       : vector to be split
+   * \param x1map   : map of first vector to be extracted
+   * \param x1      : first vector to be extracted
+   * \param x2map   : map of second vector to be extracted
+   * \param x2      : second vector to be extracted
+   */
   bool split_vector(const Core::LinAlg::Map& xmap, const Core::LinAlg::Vector<double>& x,
       std::shared_ptr<Core::LinAlg::Map>& x1map, std::shared_ptr<Core::LinAlg::Vector<double>>& x1,
       std::shared_ptr<Core::LinAlg::Map>& x2map, std::shared_ptr<Core::LinAlg::Vector<double>>& x2);
 
   /*!
-   \brief split a vector into 2 non-overlapping pieces (std::shared_ptr version)
-
-   \param xmap    : map of vector to be split
-   \param x       : vector to be split
-   \param x1map   : map of first vector to be extracted
-   \param x1      : first vector to be extracted
-   \param x2map   : map of second vector to be extracted
-   \param x2      : second vector to be extracted
-
+   * \brief split a vector into 2 non-overlapping pieces (std::shared_ptr version)
+   *
+   * \param xmap    : map of vector to be split
+   * \param x       : vector to be split
+   * \param x1map   : map of first vector to be extracted
+   * \param x1      : first vector to be extracted
+   * \param x2map   : map of second vector to be extracted
+   * \param x2      : second vector to be extracted
    */
   bool split_vector(const Core::LinAlg::Map& xmap, const Core::LinAlg::Vector<double>& x,
       std::shared_ptr<const Core::LinAlg::Map>& x1map,
@@ -319,46 +318,46 @@ namespace Core::LinAlg
       std::vector<double>& std_vector, const int block_size);
 
   /*! \brief  Transform the row map of a matrix (parallel distribution)
-
-  *
-  *  This method changes the row map of an input matrix to new row map with identical GIDs but
-  *  different parallel distribution.
-  *
-  *  \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
-  *  \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
-  *
-  *  \returns A Core::LinAlg::SparseMatrix with the rowmap given by newrowmap
-  */
+   *
+   *
+   * This method changes the row map of an input matrix to new row map with identical GIDs but
+   * different parallel distribution.
+   *
+   * \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
+   * \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
+   *
+   * \returns A Core::LinAlg::SparseMatrix with the rowmap given by newrowmap
+   */
   std::shared_ptr<Core::LinAlg::SparseMatrix> matrix_row_transform(
       const Core::LinAlg::SparseMatrix& inmat, const Core::LinAlg::Map& newrowmap);
 
   /*! \brief  Transform the column map of a matrix (parallel distribution)
-
-  *
-  *  This method changes the column map of an input matrix to new column map with identical GIDs but
-  *  different parallel distribution. (and the domain map, accordingly).
-  *
-  *  \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
-  *  \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
-  *
-  *  \returns A Core::LinAlg::SparseMatrix with the domainmap given by newdomainmap
-  */
+   *
+   *
+   *  This method changes the column map of an input matrix to new column map with identical GIDs
+   * but different parallel distribution. (and the domain map, accordingly).
+   *
+   *  \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
+   *  \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
+   *
+   *  \returns A Core::LinAlg::SparseMatrix with the domainmap given by newdomainmap
+   */
   std::shared_ptr<Core::LinAlg::SparseMatrix> matrix_col_transform(
       const Core::LinAlg::SparseMatrix& inmat, const Core::LinAlg::Map& newdomainmap);
 
   /*! \brief  Transform the row and column maps of a matrix (parallel distribution)
-
-  *
-  *  This method changes the row and column maps of an input matrix to new row and column maps with
-  *  identical GIDs but different parallel distribution (and the domain map, accordingly).
-  *
-  *  \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
-  *  \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
-  *  \param(in) newdomainmap: A Core::LinAlg::Map providing the domainmap of the new distribution.
-  *
-  *  \returns A Core::LinAlg::SparseMatrix with the rowmap given by newrowmap and domainmap
-  *   given by newdomainmap
-  */
+   *
+   *
+   * This method changes the row and column maps of an input matrix to new row and column maps with
+   * identical GIDs but different parallel distribution (and the domain map, accordingly).
+   *
+   * \param(in) inmat:  A Core::LinAlg::SparseMatrix that needs to be transformed.
+   * \param(in) newrowmap: A Core::LinAlg::Map providing the rowmap of the new distribution.
+   * \param(in) newdomainmap: A Core::LinAlg::Map providing the domainmap of the new distribution.
+   *
+   * \returns A Core::LinAlg::SparseMatrix with the rowmap given by newrowmap and domainmap
+   * given by newdomainmap
+   */
   std::shared_ptr<Core::LinAlg::SparseMatrix> matrix_row_col_transform(
       const Core::LinAlg::SparseMatrix& inmat, const Core::LinAlg::Map& newrowmap,
       const Core::LinAlg::Map& newdomainmap);
