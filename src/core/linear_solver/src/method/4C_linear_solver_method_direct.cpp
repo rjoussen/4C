6f29bea93694581878bef29786bd522a01229a2e
Max Firmbach <max.firmbach@unibw.de>
2025-08-27 17:28:01 +0200

    Refactor calls in direct solver interface

--- src/core/linear_solver/src/method/4C_linear_solver_method_direct.cpp
+++ src/core/linear_solver/src/method/4C_linear_solver_method_direct.cpp
@@ -72,7 +72,7 @@ void Core::LinearSolver::DirectSolver::setup(std::shared_ptr<Core::LinAlg::Spars
 #else
   linear_problem_->SetRHS(&b_->get_epetra_multi_vector());
   linear_problem_->SetLHS(&x_->get_epetra_multi_vector());
-  linear_problem_->SetOperator(a_->epetra_operator().get());
+  linear_problem_->SetOperator(a_->epetra_matrix().get());
 
   if (reindexer_ and not(reset or refactor)) reindexer_->fwd();
 #endif
@@ -123,7 +123,7 @@ void Core::LinearSolver::DirectSolver::setup(std::shared_ptr<Core::LinAlg::Spars
 
 #if FOUR_C_TRILINOS_INTERNAL_VERSION_GE(2025, 3)
     solver_ = Amesos2::create<Epetra_CrsMatrix, Epetra_MultiVector>(solver_type,
-        Teuchos::rcp_dynamic_cast<Epetra_CrsMatrix>(Teuchos::rcpFromRef(*a_->epetra_operator())),
+        Teuchos::rcpFromRef(*a_->epetra_matrix()),
         Teuchos::rcpFromRef(x_->get_epetra_multi_vector()),
         Teuchos::rcpFromRef(b_->get_epetra_multi_vector()));
 
